aa06a0c67f27a00d85496cb054a52bee1baf7158
KucingGatel <williamsyt2005@gmail.com>
2025-10-22 10:27:33 +0800

    king

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('User/Index', [
+            'canLogin' => app('router')->has('login'),
+            'canRegister' => app('router')->has('register'),
+        ]);
+    }
+}
