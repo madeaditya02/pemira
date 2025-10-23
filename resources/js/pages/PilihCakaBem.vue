63355ee2ab1092c3fadafe78dbb0a5154cd8b4fd
KucingGatel <williamsyt2005@gmail.com>
2025-10-24 00:57:21 +0800

    fix row pemilihan caka BEM

--- resources/js/pages/PilihCakaBem.vue
+++ resources/js/pages/PilihCakaBem.vue
@@ -132,90 +132,96 @@ const breadcrumbs: BreadcrumbItem[] = [
                 <img src="/images/bannercaka.svg" alt="" class="absolute inset-0 top-0 w-full object-cover" />
 
                 <div class="relative mx-auto mt-20 grid w-full max-w-7xl items-start gap-4 px-4 lg:mt-56">
-                    <!-- Caka 1 -->
-                    <div class=" max-w-md flex flex-col items-center justify-center gap-6 p-6">
-                        <img src="/images/Caka1.png" alt="Caka1" class="w-md" />
-                        <div class="justify-center items-center">
-                            <AlertDialog>
-                                <AlertDialogTrigger as-child>
-                                    <Button> Vote </Button>
-                                </AlertDialogTrigger>
-                                <AlertDialogContent>
-                                    <AlertDialogHeader>
-                                        <AlertDialogTitle class="flex flex-col items-center justify-center gap-2">
-                                            <TriangleAlert class="h-24 w-24 text-red-700" />
-                                            <h1 class="text-xl font-black text-red-700">ATTENTION</h1>
-                                        </AlertDialogTitle>
-                                        <AlertDialogDescription class="text-md text-center">
-                                            Pemilihan hanya dapat dilakukan sekali dan tidak ada pengulangan, apabila
-                                            anda
-                                            ingin
-                                            melanjutkan silahkan klik
-                                            tombol "Lanjutkan"!
-                                        </AlertDialogDescription>
-                                    </AlertDialogHeader>
-                                    <AlertDialogFooter class="sm:justify-center">
-                                        <div class="grid w-full grid-cols-2 gap-3">
-                                            <AlertDialogCancel
-                                                class="m-0 border border-[#5465D1] text-[#5465D1] hover:bg-[#5465D1]/10">
-                                                Batal
-                                            </AlertDialogCancel>
-                                            <AlertDialogAction as-child class="m-0">
-                                                <Link variant="outline" @click="() => {
-                                                    toast('Pilihan berhasil disimpan', {
-                                                        description: 'Sunday, December 03, 2023 at 9:00 AM',
-                                                    })
-                                                }" href="#" class="bg-[#5465D1] hover:bg-[#5465D1]/90">
-                                                    Lanjutkan
-                                                </Link>
-                                            </AlertDialogAction>
-                                        </div>
-                                    </AlertDialogFooter>
-                                </AlertDialogContent>
-                            </AlertDialog>
+                    <div class="flex flex-col md:flex-row justify-center">
+                        <!-- Caka 1 -->
+                        <div>
+                            <div class=" max-w-md flex flex-col items-center justify-center gap-6 p-6">
+                                <img src="/images/Caka1.png" alt="Caka1" class="w-md" />
+                                <div class="justify-center items-center">
+                                    <AlertDialog>
+                                        <AlertDialogTrigger as-child>
+                                            <Button> Vote </Button>
+                                        </AlertDialogTrigger>
+                                        <AlertDialogContent>
+                                            <AlertDialogHeader>
+                                                <AlertDialogTitle class="flex flex-col items-center justify-center gap-2">
+                                                    <TriangleAlert class="h-24 w-24 text-red-700" />
+                                                    <h1 class="text-xl font-black text-red-700">ATTENTION</h1>
+                                                </AlertDialogTitle>
+                                                <AlertDialogDescription class="text-md text-center">
+                                                    Pemilihan hanya dapat dilakukan sekali dan tidak ada pengulangan, apabila
+                                                    anda
+                                                    ingin
+                                                    melanjutkan silahkan klik
+                                                    tombol "Lanjutkan"!
+                                                </AlertDialogDescription>
+                                            </AlertDialogHeader>
+                                            <AlertDialogFooter class="sm:justify-center">
+                                                <div class="grid w-full grid-cols-2 gap-3">
+                                                    <AlertDialogCancel
+                                                        class="m-0 border border-[#5465D1] text-[#5465D1] hover:bg-[#5465D1]/10">
+                                                        Batal
+                                                    </AlertDialogCancel>
+                                                    <AlertDialogAction as-child class="m-0">
+                                                        <Link variant="outline" @click="() => {
+                                                            toast('Pilihan berhasil disimpan', {
+                                                                description: 'Sunday, December 03, 2023 at 9:00 AM',
+                                                            })
+                                                        }" href="#" class="bg-[#5465D1] hover:bg-[#5465D1]/90">
+                                                            Lanjutkan
+                                                        </Link>
+                                                    </AlertDialogAction>
+                                                </div>
+                                            </AlertDialogFooter>
+                                        </AlertDialogContent>
+                                    </AlertDialog>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <!-- Caka 2 -->
-                    <div class=" max-w-md flex flex-col items-center justify-center gap-6 p-6">
-                        <img src="/images/Caka1.png" alt="Caka1" class="w-md" />
-                        <div class="justify-center items-center">
-                            <AlertDialog>
-                                <AlertDialogTrigger as-child>
-                                    <Button> Vote </Button>
-                                </AlertDialogTrigger>
-                                <AlertDialogContent>
-                                    <AlertDialogHeader>
-                                        <AlertDialogTitle class="flex flex-col items-center justify-center gap-2">
-                                            <TriangleAlert class="h-24 w-24 text-red-700" />
-                                            <h1 class="text-xl font-black text-red-700">ATTENTION</h1>
-                                        </AlertDialogTitle>
-                                        <AlertDialogDescription class="text-md text-center">
-                                            Pemilihan hanya dapat dilakukan sekali dan tidak ada pengulangan, apabila
-                                            anda
-                                            ingin
-                                            melanjutkan silahkan klik
-                                            tombol "Lanjutkan"!
-                                        </AlertDialogDescription>
-                                    </AlertDialogHeader>
-                                    <AlertDialogFooter class="sm:justify-center">
-                                        <div class="grid w-full grid-cols-2 gap-3">
-                                            <AlertDialogCancel
-                                                class="m-0 border border-[#5465D1] text-[#5465D1] hover:bg-[#5465D1]/10">
-                                                Batal
-                                            </AlertDialogCancel>
-                                            <AlertDialogAction variant="outline" @click="() => {
-                                                toast('Pilihan berhasil disimpan', {
-                                                    description: 'Sunday, December 03, 2023 at 9:00 AM',
-                                                })
-                                            }" as-child class="m-0">
-                                                <Link href="#" class="bg-[#5465D1] hover:bg-[#5465D1]/90">
-                                                Lanjutkan
-                                                </Link>
-                                            </AlertDialogAction>
-                                        </div>
-                                    </AlertDialogFooter>
-                                </AlertDialogContent>
-                            </AlertDialog>
+                        <div>
+                            <!-- Caka 2 -->
+                            <div class=" max-w-md flex flex-col items-center justify-center gap-6 p-6">
+                                <img src="/images/Caka1.png" alt="Caka1" class="w-md" />
+                                <div class="justify-center items-center">
+                                    <AlertDialog>
+                                        <AlertDialogTrigger as-child>
+                                            <Button> Vote </Button>
+                                        </AlertDialogTrigger>
+                                        <AlertDialogContent>
+                                            <AlertDialogHeader>
+                                                <AlertDialogTitle class="flex flex-col items-center justify-center gap-2">
+                                                    <TriangleAlert class="h-24 w-24 text-red-700" />
+                                                    <h1 class="text-xl font-black text-red-700">ATTENTION</h1>
+                                                </AlertDialogTitle>
+                                                <AlertDialogDescription class="text-md text-center">
+                                                    Pemilihan hanya dapat dilakukan sekali dan tidak ada pengulangan, apabila
+                                                    anda
+                                                    ingin
+                                                    melanjutkan silahkan klik
+                                                    tombol "Lanjutkan"!
+                                                </AlertDialogDescription>
+                                            </AlertDialogHeader>
+                                            <AlertDialogFooter class="sm:justify-center">
+                                                <div class="grid w-full grid-cols-2 gap-3">
+                                                    <AlertDialogCancel
+                                                        class="m-0 border border-[#5465D1] text-[#5465D1] hover:bg-[#5465D1]/10">
+                                                        Batal
+                                                    </AlertDialogCancel>
+                                                    <AlertDialogAction variant="outline" @click="() => {
+                                                        toast('Pilihan berhasil disimpan', {
+                                                            description: 'Sunday, December 03, 2023 at 9:00 AM',
+                                                        })
+                                                    }" as-child class="m-0">
+                                                        <Link href="#" class="bg-[#5465D1] hover:bg-[#5465D1]/90">
+                                                        Lanjutkan
+                                                        </Link>
+                                                    </AlertDialogAction>
+                                                </div>
+                                            </AlertDialogFooter>
+                                        </AlertDialogContent>
+                                    </AlertDialog>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
