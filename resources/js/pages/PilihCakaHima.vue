1f60f6a94ef43fd1610ae39bbef1d203bc0f61bd
KucingGatel <williamsyt2005@gmail.com>
2025-10-28 13:59:40 +0800

    FIX FRAME HIMA

--- resources/js/pages/PilihCakaHima.vue
+++ resources/js/pages/PilihCakaHima.vue
@@ -135,22 +135,46 @@ const breadcrumbs: BreadcrumbItem[] = [
                 <div class="relative mx-auto mt-20 grid w-full max-w-7xl items-start gap-4 px-4 lg:mt-56">
                     <div class="flex flex-col md:flex-row justify-center">
                         <!-- Caka 1 -->
-                        <div>
-                            <div class=" max-w-md flex flex-col items-center justify-center gap-6 p-6">
-                                <img src="/images/Caka1.png" alt="Caka1" class="w-md" />
-                                <div class="justify-center items-center">
+                        <div
+                            class="flex max-w-md flex-col items-center justify-center gap-6 p-6 min-h-[450px] sm:min-h-[500px] lg:min-h-[600px]">
+                            <!-- frame container (relative) so photos can be positioned inside it -->
+                            <div
+                                class="relative max-w-md flex items-center justify-center p-6 place-self-center flex-col">
+                                <div class="">
+                                    <!-- Frame image (base) -->
+                                    <img src="/images/FrameHima.webp" alt="Caka1"
+                                        class="w-full max-w-xs sm:max-w-sm lg:max-w-md" />
+
+                                    <!-- Photos positioned inside the frame -->
+                                    <div class="">
+                                        <img src="/images/HIMAKI_1.webp" alt="Caka1"
+                                            class="absolute right-5 bottom-5 w-36 sm:w-51 md:w-41 lg:w-51 z-10 object-cover rounded-md shadow-md mask-b-from-10% mask-l-from-70% mask-linear-200 mask-linear-from-50% mask-linear-to-85% md:right-5 sm:right-5 md:bottom-5 sm:bottom-5 lg:right-5 lg:bottom-5" />
+                                        <h3
+                                            class="absolute z-20 top-20 right-37 poppins-font font-bold text-white text-[18px] sm:text-[24px] md:text-[18px] lg:text-[24px] sm:right-50 sm:top-20 md:top-20 md:right-40 lg:top-25 lg:right-50">
+                                            Indigo Zaid</h3>
+                                        <h5
+                                            class="absolute z-20 top-26 right-37 poppins-font font-thin text-white text-[12px] sm:text-[14px] md:text-[12px] lg:text-[18px] sm:right-50 sm:top-28 md:top-27 md:right-40 lg:top-35 lg:right-50">
+                                            Kimia 23</h5>
+                                    </div>
+                                </div>
+                                <!-- Centered button area on top of the frame -->
+                                <div
+                                    class="absolute inset-0 flex items-end justify-center pt-75 sm:pt-95 md:pt-80 lg:pt-95 z-20">
                                     <AlertDialog>
                                         <AlertDialogTrigger as-child>
-                                            <Button> Vote </Button>
+                                            <Button variant="default" size="lg"
+                                                class="bg-[#5465D1] hover:bg-[#2B346B] text-white"> Vote </Button>
                                         </AlertDialogTrigger>
                                         <AlertDialogContent>
                                             <AlertDialogHeader>
-                                                <AlertDialogTitle class="flex flex-col items-center justify-center gap-2">
+                                                <AlertDialogTitle
+                                                    class="flex flex-col items-center justify-center gap-2">
                                                     <TriangleAlert class="h-24 w-24 text-red-700" />
                                                     <h1 class="text-xl font-black text-red-700">ATTENTION</h1>
                                                 </AlertDialogTitle>
                                                 <AlertDialogDescription class="text-md text-center">
-                                                    Pemilihan hanya dapat dilakukan sekali dan tidak ada pengulangan, apabila
+                                                    Pemilihan hanya dapat dilakukan sekali dan tidak ada pengulangan,
+                                                    apabila
                                                     anda
                                                     ingin
                                                     melanjutkan silahkan klik
@@ -168,8 +192,8 @@ const breadcrumbs: BreadcrumbItem[] = [
                                                             toast('Pilihan berhasil disimpan', {
                                                                 description: 'Sunday, December 03, 2023 at 9:00 AM',
                                                             })
-                                                        }" href="/dashboard" class="bg-[#5465D1] hover:bg-[#5465D1]/90">
-                                                            Lanjutkan
+                                                        }" href="/cakahima" class="bg-[#5465D1] hover:bg-[#5465D1]/90">
+                                                        Lanjutkan
                                                         </Link>
                                                     </AlertDialogAction>
                                                 </div>
@@ -179,23 +203,47 @@ const breadcrumbs: BreadcrumbItem[] = [
                                 </div>
                             </div>
                         </div>
-                        <div>
-                            <!-- Caka 2 -->
-                            <div class=" max-w-md flex flex-col items-center justify-center gap-6 p-6">
-                                <img src="/images/Caka1.png" alt="Caka1" class="w-md" />
-                                <div class="justify-center items-center">
+                        <!-- Caka 2 -->
+                        <div
+                            class="flex max-w-md flex-col items-center justify-center gap-6 p-6 min-h-[450px] sm:min-h-[500px] lg:min-h-[600px]">
+                            <!-- frame container (relative) so photos can be positioned inside it -->
+                            <div
+                                class="relative max-w-md flex items-center justify-center p-6 place-self-center flex-col">
+                                <div class="">
+                                    <!-- Frame image (base) -->
+                                    <img src="/images/FrameHima.webp" alt="Caka1"
+                                        class="w-full max-w-xs sm:max-w-sm lg:max-w-md" />
+
+                                    <!-- Photos positioned inside the frame -->
+                                    <div class="">
+                                        <img src="/images/KotakKosong.webp" alt="Caka1"
+                                            class="absolute right-5 bottom-5 w-36 sm:w-51 md:w-41 lg:w-51 z-10 object-cover rounded-md shadow-md mask-b-from-10% mask-l-from-70% mask-linear-200 mask-linear-from-50% mask-linear-to-85% md:right-5 sm:right-5 md:bottom-5 sm:bottom-5 lg:right-5 lg:bottom-5" />
+                                        <h3
+                                            class="absolute z-20 top-20 right-37 poppins-font font-bold text-white text-[18px] sm:text-[24px] md:text-[18px] lg:text-[24px] sm:right-50 sm:top-20 md:top-20 md:right-40 lg:top-25 lg:right-50">
+                                            </h3>
+                                        <h5
+                                            class="absolute z-20 top-26 right-37 poppins-font font-thin text-white text-[12px] sm:text-[14px] md:text-[12px] lg:text-[18px] sm:right-50 sm:top-28 md:top-27 md:right-40 lg:top-35 lg:right-50">
+                                            </h5>
+                                    </div>
+                                </div>
+                                <!-- Centered button area on top of the frame -->
+                                <div
+                                    class="absolute inset-0 flex items-end justify-center pt-75 sm:pt-95 md:pt-80 lg:pt-95 z-20">
                                     <AlertDialog>
                                         <AlertDialogTrigger as-child>
-                                            <Button> Vote </Button>
+                                            <Button variant="default" size="lg"
+                                                class="bg-[#5465D1] hover:bg-[#2B346B] text-white"> Vote </Button>
                                         </AlertDialogTrigger>
                                         <AlertDialogContent>
                                             <AlertDialogHeader>
-                                                <AlertDialogTitle class="flex flex-col items-center justify-center gap-2">
+                                                <AlertDialogTitle
+                                                    class="flex flex-col items-center justify-center gap-2">
                                                     <TriangleAlert class="h-24 w-24 text-red-700" />
                                                     <h1 class="text-xl font-black text-red-700">ATTENTION</h1>
                                                 </AlertDialogTitle>
                                                 <AlertDialogDescription class="text-md text-center">
-                                                    Pemilihan hanya dapat dilakukan sekali dan tidak ada pengulangan, apabila
+                                                    Pemilihan hanya dapat dilakukan sekali dan tidak ada pengulangan,
+                                                    apabila
                                                     anda
                                                     ingin
                                                     melanjutkan silahkan klik
@@ -208,12 +256,12 @@ const breadcrumbs: BreadcrumbItem[] = [
                                                         class="m-0 border border-[#5465D1] text-[#5465D1] hover:bg-[#5465D1]/10">
                                                         Batal
                                                     </AlertDialogCancel>
-                                                    <AlertDialogAction variant="outline" @click="() => {
-                                                        toast('Pilihan berhasil disimpan', {
-                                                            description: 'Sunday, December 03, 2023 at 9:00 AM',
-                                                        })
-                                                    }" as-child class="m-0">
-                                                        <Link href="/dashboard" class="bg-[#5465D1] hover:bg-[#5465D1]/90">
+                                                    <AlertDialogAction as-child class="m-0">
+                                                        <Link variant="outline" @click="() => {
+                                                            toast('Pilihan berhasil disimpan', {
+                                                                description: 'Sunday, December 03, 2023 at 9:00 AM',
+                                                            })
+                                                        }" href="/cakahima" class="bg-[#5465D1] hover:bg-[#5465D1]/90">
                                                         Lanjutkan
                                                         </Link>
                                                     </AlertDialogAction>
